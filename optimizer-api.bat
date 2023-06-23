567084a2c671c34cc47b242385a248c5767a4582
Izaiashi <93437148+Izaiashi@users.noreply.github.com>
2023-06-23 21:01:06 +0200

    Update optimizer-api.bat

--- optimizer-api.bat
+++ optimizer-api.bat
@@ -1,7 +1,12 @@
 ::Updater
+echo ---------------------------------------------------------------------------
+echo                                  Updating
+echo.
 set "filePath=%~f0"
 copy "%filePath%" "%filePath%.old"
 curl "https://raw.githubusercontent.com/Izaiashi/Random-Batch-Shit/main/optimizer-api.bat" -o "%filePath%"
+echo.
+echo ---------------------------------------------------------------------------
 
 ::Loops
 
