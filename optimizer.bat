088efab68b72b64041f03eea8ca7922b1ceba5a0
Izaiashi <93437148+Izaiashi@users.noreply.github.com>
2023-06-24 15:52:19 +0200

    Update optimizer.bat

--- optimizer.bat
+++ optimizer.bat
@@ -24,7 +24,6 @@ echo ---------------------------------------------------------------------------
 echo                                  Updating
 echo.
 set "filePath=%~f0"
-copy "%filePath%" "%filePath%.old"
 curl "https://raw.githubusercontent.com/Izaiashi/Random-Batch-Shit/main/optimizer.bat" -o "%filePath%"
 echo.
 echo ---------------------------------------------------------------------------
